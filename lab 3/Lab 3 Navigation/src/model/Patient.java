9c34271f6030c947ed472a98f212b26088234ed7
Marcia <ofosuhene.m@northeastern.edu>
2024-10-11 15:05:23 -0400

    lab 3F

---
+++ lab 3/Lab 3 Navigation/src/model/Patient.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author yiado
+ */
+public class Patient {
+    private String firstname;
+    private String lastname;
+    private String gender;
+    private String patientType;
+    private ImageIcon profilePicture;
+    private int age;
+    private String email;
+    
+    public String getLastname()
+        
+            return Lastname;
+    
+}
